50b6b562a9c3ce96f8a581893082496ccb76d0f9
Greg Kamradt <gkamradt@gmail.com>
2023-12-07 07:16:34 -0800

    first commit, working simple splitter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import defaultText from './defaultText.js';
 
 function App() {
+  const [text, setText] = useState(defaultText);
+  const [chunkSize, setChunkSize] = useState(10);
+  const [chunkCount, setChunkCount] = useState(0);
+  const [averageSize, setAverageSize] = useState(0);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSliderChange = (event) => {
+    setChunkSize(Number(event.target.value));
+  };
+
+  const simpleCharacterSplitter = ({ text, parameters }) => {
+    const [chunkSize] = parameters;
+    let result = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+      const chunk = text.substring(i, i + chunkSize);
+      result.push(chunk);
+    }
+    return result;
+  };
+
+  useEffect(() => {
+    let totalSize = 0;
+    let count = 0;
+    for (let i = 0; i < text.length; i += chunkSize) {
+      const chunk = text.substring(i, i + chunkSize);
+      totalSize += chunk.length;
+      count++;
+    }
+    setChunkCount(count);
+    setAverageSize(count > 0 ? totalSize / count : 0);
+  }, [text, chunkSize]);
+
+  const getChunkedText = () => {
+    let result = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+      const chunk = text.substring(i, i + chunkSize);
+      const key = `chunk-${i}`;
+      const style = { backgroundColor: `hsl(${(i/chunkSize) * 60}, 70%, 80%)` };
+      result.push(<span key={key} style={style}>{chunk}</span>);
+    }
+    return result;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>chunk viz v0.1</h1>
+      <textarea value={text} onChange={handleTextChange} rows={10} cols={50} />
+      <div>
+        <label>
+          Chunk Size: {chunkSize}
+          <input type="range" min="1" max="500" value={chunkSize} onChange={handleSliderChange} />
+        </label>
+      </div>
+      <div>
+        Number of Chunks: {chunkCount}, Average Size: {averageSize.toFixed(0)}
+      </div>
+      <div className="chunked-text">
+        {getChunkedText()}
+      </div>
     </div>
   );
 }
---
+++ src/defaultText.js
@@ -0,0 +1,25 @@
+const defaultText = `One of the most important things I didn't understand about the world when I was a child is the degree to which the returns for performance are superlinear.
+
+Teachers and coaches implicitly told us the returns were linear. "You get out," I heard a thousand times, "what you put in." They meant well, but this is rarely true. If your product is only half as good as your competitor's, you don't get half as many customers. You get no customers, and you go out of business.
+
+It's obviously true that the returns for performance are superlinear in business. Some think this is a flaw of capitalism, and that if we changed the rules it would stop being true. But superlinear returns for performance are a feature of the world, not an artifact of rules we've invented. We see the same pattern in fame, power, military victories, knowledge, and even benefit to humanity. In all of these, the rich get richer. [1]
+
+You can't understand the world without understanding the concept of superlinear returns. And if you're ambitious you definitely should, because this will be the wave you surf on.
+
+
+
+
+
+It may seem as if there are a lot of different situations with superlinear returns, but as far as I can tell they reduce to two fundamental causes: exponential growth and thresholds.
+
+The most obvious case of superlinear returns is when you're working on something that grows exponentially. For example, growing bacterial cultures. When they grow at all, they grow exponentially. But they're tricky to grow. Which means the difference in outcome between someone who's adept at it and someone who's not is very great.
+
+Startups can also grow exponentially, and we see the same pattern there. Some manage to achieve high growth rates. Most don't. And as a result you get qualitatively different outcomes: the companies with high growth rates tend to become immensely valuable, while the ones with lower growth rates may not even survive.
+
+Y Combinator encourages founders to focus on growth rate rather than absolute numbers. It prevents them from being discouraged early on, when the absolute numbers are still low. It also helps them decide what to focus on: you can use growth rate as a compass to tell you how to evolve the company. But the main advantage is that by focusing on growth rate you tend to get something that grows exponentially.
+
+YC doesn't explicitly tell founders that with growth rate "you get out what you put in," but it's not far from the truth. And if growth rate were proportional to performance, then the reward for performance p over time t would be proportional to pt.
+
+Even after decades of thinking about this, I find that sentence startling.`
+
+export default defaultText;
