0d5402c0c46446e64d82c0670460ce9f9a7369eb
Greg Kamradt <gkamradt@gmail.com>
2023-12-07 15:56:08 -0800

    more chunkers

--- src/App.js
+++ src/App.js
@@ -1,70 +1,267 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import defaultText from './defaultText.js';
+import { defaultProse, defaultJS, defaultPython, defaultMarkdown } from './defaultText.js';
+import { CharacterTextSplitter, RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+class RecursiveCharacterTextSplitter_ext extends RecursiveCharacterTextSplitter {
+  joinDocs(docs, separator) {
+    // LangChain trims chunks, we don't want that for visuals!
+    return docs.join(separator);
+  }
+}
+
+class CharacterTextSplitter_ext extends CharacterTextSplitter {
+  joinDocs(docs, separator) {
+    // LangChain trims chunks, we don't want that for visuals!
+    return docs.join(separator);
+  }
+}
 
 function App() {
-  const [text, setText] = useState(defaultText);
-  const [chunkSize, setChunkSize] = useState(10);
-  const [chunkCount, setChunkCount] = useState(0);
-  const [averageSize, setAverageSize] = useState(0);
+  const [text, setText] = useState(defaultProse);
+  const [chunkSize, setChunkSize] = useState(25);
+  const [overlap, setOverlap] = useState(4);
+  const [highlightedText, setHighlightedText] = useState('');
+  const [splitter, setSplitter] = useState('characterSplitter');
+  const [rawChunks, setRawChunks] = useState([]);
 
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
+  const colors = ['#70d6ff', '#e9ff70', '#ff9770', '#ffd670', '#ff70a6'];
+  const MAX_TEXT_LENGTH = 100000; // Define your maximum text length
 
-  const handleSliderChange = (event) => {
-    setChunkSize(Number(event.target.value));
-  };
-
-  const simpleCharacterSplitter = ({ text, parameters }) => {
-    const [chunkSize] = parameters;
-    let result = [];
-    for (let i = 0; i < text.length; i += chunkSize) {
-      const chunk = text.substring(i, i + chunkSize);
-      result.push(chunk);
-    }
-    return result;
+  const splitterOptions = {
+    'characterSplitter': {
+      label: 'Character Splitter 🦜️🔗',
+      language: null,
+      chunk_overlap_ind: true,
+      defaultText: defaultProse
+    },
+    'recursiveCharacterTextSplitter': {
+      label: 'Recursive Character Text Splitter 🦜️🔗',
+      language: null,
+      chunk_overlap_ind: false,
+      defaultText: defaultProse
+    },
+    'recursiveCharacterTextSplitterJS': {
+      label: 'Recursive Character Text Splitter - JS 🦜️🔗',
+      language: 'js',
+      chunk_overlap_ind: false,
+      defaultText: defaultJS
+    },
+    'recursiveCharacterTextSplitterPython': {
+      label: 'Recursive Character Text Splitter - Python 🦜️🔗',
+      language: 'python',
+      chunk_overlap_ind: false,
+      defaultText: defaultPython
+    },
+    'recursiveCharacterTextSplitterMarkdown': {
+      label: 'Recursive Character Text Splitter - Markdown 🦜️🔗',
+      language: 'markdown',
+      chunk_overlap_ind: false,
+      defaultText: defaultMarkdown
+    },
   };
 
   useEffect(() => {
-    let totalSize = 0;
-    let count = 0;
-    for (let i = 0; i < text.length; i += chunkSize) {
-      const chunk = text.substring(i, i + chunkSize);
-      totalSize += chunk.length;
-      count++;
+    if (!splitterOptions[splitter].chunk_overlap_ind) {
+      setOverlap(0);
     }
-    setChunkCount(count);
-    setAverageSize(count > 0 ? totalSize / count : 0);
-  }, [text, chunkSize]);
 
-  const getChunkedText = () => {
-    let result = [];
-    for (let i = 0; i < text.length; i += chunkSize) {
-      const chunk = text.substring(i, i + chunkSize);
-      const key = `chunk-${i}`;
-      const style = { backgroundColor: `hsl(${(i/chunkSize) * 60}, 70%, 80%)` };
-      result.push(<span key={key} style={style}>{chunk}</span>);
+    // Get all default texts
+    const defaultTexts = Object.values(splitterOptions).map(option => option.defaultText) || [];
+
+    // Check if the current text is blank or a default text
+    if (text === '' || defaultTexts.includes(text)) {
+      setText(splitterOptions[splitter].defaultText); // Set the default text for the selected splitter
     }
-    return result;
+  }, [splitter, text, splitterOptions]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await renderTextWithHighlights();
+      setHighlightedText(result);
+    })();
+  }, [text, chunkSize, overlap, splitter]);
+
+  const handleTextChange = (event) => {
+    let newText = event.target.value;
+    if (newText.length > MAX_TEXT_LENGTH) {
+      alert(`Error: Text cannot be longer than ${MAX_TEXT_LENGTH} characters. It will be trimmed to fit the limit.`);
+      newText = newText.substring(0, MAX_TEXT_LENGTH);
+    }
+    setText(newText);
   };
+
+  const handleChunkSizeChange = (event) => {
+    let newChunkSize = Number(event.target.value);
+    if (newChunkSize > overlap * 2) {
+      setChunkSize(newChunkSize);
+    }
+  };
+
+  const handleOverlapChange = (event) => {
+    let newOverlap = Number(event.target.value);
+    if (newOverlap <= chunkSize * 0.5) {
+      setOverlap(newOverlap);
+    }
+  };
+
+  const reconstructChunks = (chunks, chunkOverlap) => {
+    let reconstructedText = [];
+    let chunkData = [];
+    let currentStartIndex = 0;
+
+    chunks.forEach((chunk, index) => {
+      const isLastChunk = index === chunks.length - 1;
+      const startIndex = currentStartIndex; // Adjusted start index
+      const endIndex = startIndex + chunk.length; // Adjusted end index
+
+      reconstructedText.push(chunk);
+      chunkData.push({
+        id: index + 1,
+        startIndex: startIndex,
+        endIndex: endIndex,
+        text: chunk,
+        overlapWithNext: chunkOverlap
+      });
+
+      currentStartIndex = endIndex - (isLastChunk ? 0 : chunkOverlap); // Adjusted current start index
+    });
+
+
+    return chunkData;
+  };
+
+  const highlightChunks = (chunks) => {
+    let highlightedText = '';
+    console.log("Processed Chunks", chunks);
+    chunks.forEach((chunk, index) => {
+      let uniquePart, overlapPart;
+
+      if (index === 0) {
+        uniquePart = chunk.text.slice(0, chunk.text.length - chunk.overlapWithNext);
+        overlapPart = chunk.text.slice(chunk.text.length - chunk.overlapWithNext);
+      } else if (index !== chunks.length - 1) {
+        uniquePart = chunk.text.slice(chunk.overlapWithNext, chunk.text.length - chunk.overlapWithNext);
+        overlapPart = chunk.text.slice(chunk.text.length - chunk.overlapWithNext, chunk.text.overlapWithNext);
+      } else { // It's the last chunk
+        uniquePart = chunk.text.slice(chunk.overlapWithNext);
+        overlapPart = ''; // There's no overlap with the next chunk
+      }
+
+      // Generate a pseudo-random color for each unique part using HSL
+      const color = colors[index % colors.length];
+
+      const highlightedChunk = `<span style="background: ${color}">${uniquePart}</span>`;
+      highlightedText += highlightedChunk;
+
+      // Add overlap part only if it's not the last chunk
+      if (overlapPart) {
+        highlightedText += `<span class="overlap">${overlapPart}</span>`;
+      }
+    });
+    return highlightedText;
+  };
+
+  const chunkTextSimple = async (text, chunkSize, overlap) => {
+    const splitter = new CharacterTextSplitter_ext({
+      separator: "",
+      chunkSize: chunkSize,
+      chunkOverlap: overlap,
+      keepSeparator: true
+    });
+
+    const documents = await splitter.createDocuments([text]);
+
+    let chunks = []
+    for (let document of documents) {
+      chunks.push(document.pageContent);
+    }
+    return chunks || []; // Ensure that an array is returned
+  };
+
+  const chunkTextRecursive = async (text, chunkSize, overlap, language) => {
+    let splitter;
+    if (language) {
+      splitter = RecursiveCharacterTextSplitter_ext.fromLanguage(language, {
+        chunkSize: chunkSize,
+        chunkOverlap: overlap,
+        keepSeparator: true
+      });
+    } else {
+      console.log("regular recurssive", chunkSize, overlap)
+      splitter = new RecursiveCharacterTextSplitter_ext({
+        chunkSize: chunkSize,
+        chunkOverlap: overlap,
+        keepSeparator: true
+      });
+    }
+
+    const documents = await splitter.createDocuments([text]);
+
+    let chunks = []
+    for (let document of documents) {
+      chunks.push(document.pageContent);
+    }
+    return chunks;
+  };
+
+  const renderTextWithHighlights = async () => {
+    let rawChunks;
+    const language = splitterOptions[splitter].language;
+    if (splitter.startsWith('characterSplitter')) {
+      rawChunks = await chunkTextSimple(text, chunkSize, overlap);
+    } else {
+      rawChunks = await chunkTextRecursive(text, chunkSize, overlap, language);
+    }
+    console.log("Raw Chunks From LangChain", rawChunks)
+    setRawChunks(rawChunks); // Set the state variable
+    const reconstructedChunks = reconstructChunks(rawChunks, overlap);
+    const highlightedText = highlightChunks(reconstructedChunks);
+    return highlightedText;
+  };
+
+  console.log(chunkSize)
 
   return (
     <div className="App">
-      <h1>chunk viz v0.1</h1>
+      <h1>ChunkViz v0.1</h1>
+      <p>This is an exploratory tool to visualize how common text splitters work</p>
+      <p>For implementations of text splitters, view LangChain (py, js) & Llama Index (py, js)</p>
       <textarea value={text} onChange={handleTextChange} rows={10} cols={50} />
       <div>
+        <div>
+          <label>
+            Splitter:
+            <select value={splitter} onChange={(e) => setSplitter(e.target.value)}>
+              {Object.entries(splitterOptions).map(([value, { label }]) => (
+                <option key={value} value={value}>{label}</option>
+              ))}
+            </select>
+          </label>
+        </div>
         <label>
-          Chunk Size: {chunkSize}
-          <input type="range" min="1" max="500" value={chunkSize} onChange={handleSliderChange} />
+          Chunk Size {chunkSize}:
+          <input type="range" min="1" max="2000" value={chunkSize} onChange={handleChunkSizeChange} />
         </label>
-      </div>
-      <div>
-        Number of Chunks: {chunkCount}, Average Size: {averageSize.toFixed(0)}
+        <label>
+          Overlap {overlap}:
+          <input type="range" min="0" max="200" value={overlap} onChange={handleOverlapChange} disabled={!splitterOptions[splitter].chunk_overlap_ind} />
+        </label>
+        <div>
+          Total Characters: {rawChunks.reduce((a, b) => a + b.length, 0)}
+        </div>
+        <div>
+          Number of chunks: {rawChunks.length}
+        </div>
+        <div>
+          Average chunk size: {(rawChunks.reduce((a, b) => a + b.length, 0) / rawChunks.length).toFixed(1)}
+        </div>
       </div>
       <div className="chunked-text">
-        {getChunkedText()}
+        <div dangerouslySetInnerHTML={{ __html: highlightedText }} />
       </div>
+      <p>Made with ❤️ by <a href="https://twitter.com/GregKamradt" target="_blank" rel="noopener noreferrer">Greg Kamradt</a></p>
+      <p><a href="https://github.com/gkamradt" target="_blank" rel="noopener noreferrer">PRs Welcome</a></p>
     </div>
   );
 }
--- src/defaultText.js
+++ src/defaultText.js
@@ -1,14 +1,10 @@
-const defaultText = `One of the most important things I didn't understand about the world when I was a child is the degree to which the returns for performance are superlinear.
+const defaultProse = `One of the most important things I didn't understand about the world when I was a child is the degree to which the returns for performance are superlinear.
 
 Teachers and coaches implicitly told us the returns were linear. "You get out," I heard a thousand times, "what you put in." They meant well, but this is rarely true. If your product is only half as good as your competitor's, you don't get half as many customers. You get no customers, and you go out of business.
 
 It's obviously true that the returns for performance are superlinear in business. Some think this is a flaw of capitalism, and that if we changed the rules it would stop being true. But superlinear returns for performance are a feature of the world, not an artifact of rules we've invented. We see the same pattern in fame, power, military victories, knowledge, and even benefit to humanity. In all of these, the rich get richer. [1]
 
 You can't understand the world without understanding the concept of superlinear returns. And if you're ambitious you definitely should, because this will be the wave you surf on.
-
-
-
-
 
 It may seem as if there are a lot of different situations with superlinear returns, but as far as I can tell they reduce to two fundamental causes: exponential growth and thresholds.
 
@@ -22,4 +18,79 @@ YC doesn't explicitly tell founders that with growth rate "you get out what you 
 
 Even after decades of thinking about this, I find that sentence startling.`
 
-export default defaultText;
+const defaultJS = `import { Document } from "langchain/document";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+const text = "Some other considerations include:
+
+- Do you deploy your backend and frontend together, or separately?
+- Do you deploy your backend co-located with your database, or separately?
+
+**Production Support:** As you move your LangChains into production, we'd love to offer more hands-on support.
+Fill out [this form](https://airtable.com/appwQzlErAS2qiP0L/shrGtGaVBVAz7NcV2) to share more about what you're building, and our team will get in touch.
+
+## Deployment Options
+
+See below for a list of deployment options for your LangChain app. If you don't see your preferred option, please get in touch and we can add it to this list.";
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 50,
+  chunkOverlap: 1,
+  separators: ["|", "##", ">", "-"],
+});
+
+const docOutput = await splitter.splitDocuments([
+  new Document({ pageContent: text }),
+]);
+
+console.log(docOutput);`
+
+const defaultPython = `from operator import itemgetter
+
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.vectorstores import FAISS
+
+vectorstore = FAISS.from_texts(
+    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+model = ChatOpenAI()
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)`
+
+const defaultMarkdown = `# Needle In A Haystack - Pressure Testing LLMs
+
+Supported model providers: OpenAI, Anthropic
+
+A simple 'needle in a haystack' analysis to test in-context retrieval ability of long context LLMs.
+
+Get the behind the scenes on the [overview video](https://youtu.be/KwRRuiCCdmc).
+
+![GPT-4-128 Context Testing](img/NeedleHaystackCodeSnippet.png)
+
+git clone https://github.com/gkamradt/LLMTest_NeedleInAHaystack.git
+
+## The Test
+1. Place a random fact or statement (the 'needle') in the middle of a long context window (the 'haystack')
+2. Ask the model to retrieve this statement
+3. Iterate over various document depths (where the needle is placed) and context lengths to measure performance
+`
+
+export { defaultProse, defaultJS, defaultPython, defaultMarkdown };
